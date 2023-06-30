eac879d86ae5a735e3fb9f866f5137a4d6359c09
Seungwoo321 <seungwoo321@gmail.com>
2023-06-30 17:46:11 +0900

    chore: add deploy script

---
+++ packages/frontend/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+
+git init 
+git config --local user.name "Seungwoo Lee"
+git config --local user.email "seungwoo321@gmail.com"
+git add -A
+git commit -m "deploy for learn-typescript-project"
+git push -f git@github.com:Seungwoo321/learn-typescript.git master:gh-pages
+rm -rf .git
